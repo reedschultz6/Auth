7f2a0ad4f192b6e0f1bd13ddc5c0863bebcdff10
Reed Schultz <reed.schultz@kellogg.northwestern.edu>
2025-02-25 21:34:38 +0000

    Authenticating

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,10 @@ class SessionsController < ApplicationController
     # 2. if the user exists -> check if they know their password
     if @user.present? 
     # 3. if they know their password -> login is successful
-    if @user ["password"] == params["password"]
+    if BCrypt::Password.new(@user["password"]) == params["password"]
+      cookies ["name"] = "Cookie Monster"
+      session["user_id"] = @user["id"]
+      
     flash["notice"] = "Welcome."
     redirect_to "/companies"
     else 
